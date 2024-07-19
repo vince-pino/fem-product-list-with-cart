d8f172f28b55122fc43cce6915f8d1e9ede65003
Vince Cedie Pino <vincecediepino@gmail.com>
2024-07-19 15:25:08 +0700

    finish responsive design

--- js/index.js
+++ js/index.js
@@ -1,38 +1,38 @@
 import { foodData } from './food-data.js';
 
 function displayAllItems() {
-  // foodData.forEach((item) => {
-  //   const itemsContainer = document.querySelector("#items-container");
-  //   itemsContainer.innerHTML += `
-  //     <div class="card relative">
-  //         <div class="bg-[url('${item.desktop}')] bg-no-repeat bg-cover h-60 w-full mb-9 rounded-lg">
-  //         </div>
-  //         <div class="text-sm font-thin">
-  //           ${item.name} 
-  //         </div>
-  //         <div class="text-base font-semibold">
-  //           ${item.description}
-  //         </div>
-  //         <div class="text-rose-700 font-bold">
-  //           $${item.price.toFixed(2)}
-  //         </div>
-  //         <button class="flex items-center justify-center bg-white w-40 h-12 rounded-full border border-gray-400 absolute top-[220px] left-1/2 -translate-x-1/2 duration-300 ease-out hover:border-rose-900 group">
-  //           <span id="decrement-btn" class="hidden">
-  //               <svg class="text-white group-hover:text-red-400" xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="currentColor" viewBox="0 0 10 2">
-  //                   <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z"/>
-  //               </svg>
-  //           </span>
-  //           <img id="cart-icon" src="assets/images/icon-add-to-cart.svg" alt="">
-  //           <span class="ml-2 font-semibold text-sm group-hover:text-[hsl(14,86%,42%)]" data-value="0" id="btn-text">
-  //             Add to cart
-  //           </span>
-  //           <span id="increment-btn" class="hidden">
-  //             <svg class="text-white group-hover:text-red-400" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" viewBox="0 0 10 10"><path fill="currentColor" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
-  //           </span>
-  //         </button>
-  //       </div>
-  //   `;
-  // });
+  foodData.forEach((item) => {
+    const itemsContainer = document.querySelector("#items-container");
+    itemsContainer.innerHTML += `
+      <div class="card relative">
+          <div id="image-container" class="bg-[url('${item.desktop}')] bg-no-repeat bg-cover h-60 w-full mb-9 rounded-lg">
+          </div>
+          <div class="text-sm font-thin">
+            ${item.name} 
+          </div>
+          <div class="text-base font-semibold">
+            ${item.description}
+          </div>
+          <div class="text-rose-700 font-bold">
+            $${item.price.toFixed(2)}
+          </div>
+          <button class="flex items-center justify-center bg-white w-40 h-12 rounded-full border border-gray-400 absolute top-[220px] left-1/2 -translate-x-1/2 duration-300 ease-out hover:border-rose-900 group">
+            <span id="decrement-btn" class="hidden">
+                <svg class="text-white group-hover:text-red-400" xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="currentColor" viewBox="0 0 10 2">
+                    <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z"/>
+                </svg>
+            </span>
+            <img id="cart-icon" src="assets/images/icon-add-to-cart.svg" alt="">
+            <span class="ml-2 font-semibold text-sm group-hover:text-[hsl(14,86%,42%)]" data-value="0" id="btn-text">
+              Add to cart
+            </span>
+            <span id="increment-btn" class="hidden">
+              <svg class="text-white group-hover:text-red-400" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" viewBox="0 0 10 10"><path fill="currentColor" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>
+            </span>
+          </button>
+        </div>
+    `;
+  });
 
   const cards = document.querySelectorAll(".card");
 
